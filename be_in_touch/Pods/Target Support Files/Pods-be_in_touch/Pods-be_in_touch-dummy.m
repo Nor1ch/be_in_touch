0bface248674086d3481b88eeadcab98e3ce29f5
Ilia Nor1 <iliacherepanin@gmail.com>
2023-05-05 13:14:49 +0300

    init

---
+++ be_in_touch/Pods/Target Support Files/Pods-be_in_touch/Pods-be_in_touch-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_be_in_touch : NSObject
+@end
+@implementation PodsDummy_Pods_be_in_touch
+@end
---
+++ be_in_touch/be_in_touch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  be_in_touch
+//
+//  Created by Nor1 on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
